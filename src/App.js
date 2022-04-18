a8de7ef2ea3c9d05484b66f71e4e88cc2d6dcd00
Víctor Olvera <vhocar@gmail.com>
2022-04-18 09:51:36 -0700

    links menu pending

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import BrandBar from './components/BrandBar/BrandBar';
+import Body from './components/Body/Body';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App flex flex-col items-center border h-screen">
+
+      <BrandBar
+        backgroundColor={ `#4D2D7F` }
+        foregroundColor={ `white` }
+        logo={ `./logo192.png` }
+        companyName={ `Nombre de la Compañía` }
+      />
+
+      <Body
+        backgroundImage={ `https://i.picsum.photos/id/128/1920/1080.jpg?hmac=A9n9Hfj2w0ohm-zgX1FrZsnp7jH_iE-NR6RJhjuMv4A` }
+        backgroundPosition={ `center` }
+        backgroundRepeat={ `no-repeat` }
+        backgroundSize={ `cover` }
+        foregroundColor={ `black` }
+      />
+
     </div>
   );
 }
